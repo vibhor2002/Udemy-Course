600011f67239558f038ecd3ec1141aa6e158d528
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-05 22:05:09 +0530

    Done

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if (n<0)
+    return-1;
+    
+    if(n==0)
+    return 1;
+
+    int smallAns =fact(n-1);
+    return n*smallAns;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int ans = fact(n);
+    cout<<ans<<endl;
+    return 0;
+}
