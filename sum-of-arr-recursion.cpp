79a95596c9a7c49de8476ba566d65fbe829b6c44
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-07 16:26:54 +0530

    Done

---
+++ sum-of-arr-recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int sumofArray(int a[],int n){
+    // Base Case
+    if(n==0){
+        return 0;
+    }
+
+    return a[0]+sumofArray(a+1,n-1);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    cout<<sumofArray(a,n);
+    return 0;
+}
