4edf429b220f85caa0f19af3166e51bf7c32ba49
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-08 22:17:36 +0530

    Done

---
+++ check-element-present-arr-recursion.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+bool func(int a[],int n,int key){
+    if(n==0){
+        return false;
+    }
+
+    if(a[0]==key){
+        return true;
+    }
+    return func(a+1,n-1,key);
+
+}
+
+bool func2(int a[],int n,int key){
+    if(n==0){
+        return false;
+    }
+
+    if(a[n-1]==key){
+        return true;
+    }
+    return func2(a,n-1,key);
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int key;
+    cin>>key;
+
+    if (func(a,n,key)){
+        cout<<"Present";
+    }
+    else{
+        cout<<"Not Present";
+    }
+    return 0;
+}
