7e2adb7fc8544fe8e9153fe8e27339e89f36616f
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-10 22:22:01 +0530

    Done

---
+++ reverse-print-recursion.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void print(char input[]){
+    if(input[0]=='\0'){
+        return;
+    }
+
+    cout<<input[0];
+    print(input+1);
+}
+
+void reversePrint(char input[]){
+    if(input[0]=='\0'){
+        return;
+    }
+
+    reversePrint(input+1);
+    cout<<input[0];
+}
+
+int main()
+{
+    char input[100];
+    cin>>input;
+
+    print(input);
+    cout<<endl;
+    reversePrint(input);
+
+    return 0;
+}
