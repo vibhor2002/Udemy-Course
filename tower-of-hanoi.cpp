5fa75611a3c17f045ab3434eb8c796494ecc9548
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-15 00:06:15 +0530

    Steps Printed

--- tower-of-hanoi.cpp
+++ tower-of-hanoi.cpp
@@ -10,12 +10,23 @@ int towerOfHanoi(int n){
     //      a->b            a->c        b->c
 }
 
+void printSteps(int n,char s,char d,char h){
+    if(n==0){
+        return;
+    }
+
+    printSteps(n-1,s,h,d);
+    cout<<"Moving Disk "<<n<<" :  "<<s<<" -> "<<d<<endl;
+    printSteps(n-1,h,d,s);
+    }
+
 int main()
 {
     int n;
     cin>>n;
 
     cout<<towerOfHanoi(n)<<endl;
-    
+    printSteps(n,'A','C','B');
+
     return 0;
 }
